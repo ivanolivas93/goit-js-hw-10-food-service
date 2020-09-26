5162ab4f0dbe75cc26c60e5b0aeece80890626fa
Juan Olivas <juanolivas0693@gmail.com>
2020-09-26 15:23:31 +0300

    module 10 done

--- food-service/src/index.js
+++ food-service/src/index.js
@@ -3,6 +3,8 @@ import './changeTheme.js';
 import menuItems from './templates/menu-items.hbs';
 import menuPositions from '../menu.json';
 
+// Шаблонизатор
+
 let menu = document.querySelector('.js-menu');
 
 function buildMenu(array) {
