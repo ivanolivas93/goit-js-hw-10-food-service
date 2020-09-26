61c3ea7f2d400ec9e2157aca425281a73de9ef8e
Juan Olivas <juanolivas0693@gmail.com>
2020-09-26 15:06:10 +0300

    module 10

---
+++ food-service/src/index.js
@@ -0,0 +1,13 @@
+import './styles.css';
+import './changeTheme.js';
+import menuItems from './templates/menu-items.hbs';
+import menuPositions from '../menu.json';
+
+let menu = document.querySelector('.js-menu');
+
+function buildMenu(array) {
+  const markup = array.map(post => menuItems(post)).join('');
+  menu.insertAdjacentHTML('beforeend', markup);
+}
+
+buildMenu(menuPositions);
